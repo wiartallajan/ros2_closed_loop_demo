244c52a8f5556619062db1169d7e7debde35edab
Jan Wiartalla <wiartalla@igmr.rwth-aachen.de>
2025-02-28 17:22:53 +0100

    ADD: provide initial version of closed loop demo repository to demonstrate unexpected Gazebo behaviour

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # ros2_closed_loop_demo
-Demo repository to test the limits of modelling closed kinematic loop in ROS2 and the new Gazebo
+
+Demo repository to test the limits of modelling closed kinematic loops in ROS2 and the new Gazebo
+
+## Install
+
+This repo works with all the standard ROS2 / Gazebo packages, no special installs required.
+
+The functionality was only tested for `ROS2 Humble` with `Gazebo Harmonic`.
+
+Simply clone the repository, link to your ros2 workspace and build.
+
+```bash
+colcon build
+```
+
+In a new terminal source the workspace (if you have not added this to your bashrc-file, make sure to source ROS as well).
+
+```bash
+. install/setup.bash
+```
+
+## Usage
+
+To launch the provided models, simply use the provided launch files.
+
+### 1. Five Bar Linkage
+
+This model contains a standard five bar linkage. The axes of the closed loop are aligned with the world coordinate axis.
+
+```bash
+ros2 launch ros2_closed_loop_demo launch_closed_loop_demo.launch.py
+```
+
+>> This model behaves as expected: The DetachableJoint plugin keeps the kinematic loop closed and the links dangle freely.
+
+### 2. Five Bar Linkage - rotated
+
+This model contains a standard five bar linkage. But the five bar linkage is rotated around the z-axis, so that the axes of the closed loop are not aligned with the world coordinate axes anymore.
+
+```bash
+ros2 launch ros2_closed_loop_demo launch_closed_loop_rotated.launch.py
+```
+
+>> This model does not behave as expected: the kinematic chain bugs around.
+
+### 3. Five Bar Linkage with additional vertical joint
+
+This model contains a standard five bar linkage. But the five bar linkage is suspended with an additional joint aligned with the world z-axis.
+
+```bash
+ros2 launch ros2_closed_loop_demo launch_closed_loop_addJoint.launch.py
+```
+
+>> This model does not behave as expected: Gazebo crashes as soon as the vertical joint D0 is moved via the JointPositionController-Plugin in Gazebo. As long as the joint is not moved after launch, the model behaves as expected.
---
+++ ros2_closed_loop_demo_bringup/launch/launch_closed_loop_addJoint.launch.py
@@ -0,0 +1,102 @@
+# Launch file to launch a closed_loop demo file
+
+import os
+
+from ament_index_python.packages import get_package_share_directory, get_package_prefix
+
+from launch import LaunchDescription
+from launch.events import Shutdown
+from launch.actions import RegisterEventHandler, ExecuteProcess, TimerAction, IncludeLaunchDescription, DeclareLaunchArgument, EmitEvent, OpaqueFunction, AppendEnvironmentVariable, SetEnvironmentVariable
+from launch.event_handlers import OnProcessExit
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution, LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.conditions import IfCondition
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+import xacro
+import sys
+import time
+import subprocess
+
+
+def generate_launch_description():
+    
+
+    ### General Setup ###
+
+
+    # Set path to bringup package (=current package)
+    bringup_pkg = 'ros2_closed_loop_demo_bringup'
+    bringup_share = FindPackageShare(package=bringup_pkg).find(bringup_pkg)
+
+    # Set path to description package and model files
+    description_pkg = 'ros2_closed_loop_demo_description'
+    description_share = FindPackageShare(package=description_pkg).find(description_pkg)
+    model_name = 'closed_loop_demo'
+    world_name = 'demo_world_addJoint'
+    world_path = os.path.join(description_share, 'worlds', world_name+'.sdf')
+    model_path = os.path.join(description_share, 'models', model_name, 'model.sdf')
+
+    # set ros_gz_sim package
+    ros_gz_sim_pkg = 'ros_gz_sim'
+    ros_gz_bridge_pkg = 'ros_gz_bridge'
+    ros_gz_sim_share = get_package_share_directory(ros_gz_sim_pkg)
+    gz_launch_path = PathJoinSubstitution([ros_gz_sim_share, 'launch', 'gz_sim.launch.py'])
+    gz_sim_path = os.path.join(description_share, 'models')
+
+
+    ### Set environment variables ###
+
+
+    # Set gazebo model path
+    set_env_vars_resources = AppendEnvironmentVariable('GZ_SIM_RESOURCE_PATH', gz_sim_path)
+    set_env_gz_version = SetEnvironmentVariable('GZ_VERSION', 'harmonic')
+
+    env_variables = [
+        set_env_vars_resources,
+        set_env_gz_version,
+    ]
+    
+
+    ### Define Launch actions ###
+
+
+    # Kill all gazebo instances
+    kill_gz_harmonic_instances_cmd = ExecuteProcess(
+        cmd=['pkill', '-f', '-9', 'ign gazebo'],
+        output='screen'
+    )
+
+    # Launch gazebo with a given world model (Gazebo Harmonic)
+    launch_gz_harmonic_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(gz_launch_path),
+        launch_arguments={
+            'gz_args': ['-r ', world_path,
+                # ' --gui-config ', PathJoinSubstitution(
+                #     [bringup_share, 'gazebo', 'gz_dec_overview.config']
+                #     ),
+                ],
+            'on_exit_shutdown': 'True'
+        }.items()
+    )
+
+
+    ### Declare launch commands ###
+
+
+    nodes = [
+        
+        kill_gz_harmonic_instances_cmd,
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=kill_gz_harmonic_instances_cmd,
+                on_exit=[launch_gz_harmonic_cmd],
+            )
+        )
+
+    ]
+    
+
+    return LaunchDescription(env_variables + nodes)
